d405dd8fcde4a0608bb7c0ed17ef43899bcc911e
Antonio Meza Pérez <solvermex@outlook.com>
2022-08-09 20:21:03 -0500

    patron adaptador

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
-import * as request from 'supertest';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios
-  
   constructor(
 
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    
+    private readonly http: AxiosAdapter
 
   ) {}
 
@@ -24,7 +23,7 @@ export class SeedService {
     await this.pokemonModel.deleteMany({})
     
     // Obtener Datos del API
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=20')
 
     // // Guardar Array
     // const insertPromisesArray = []
